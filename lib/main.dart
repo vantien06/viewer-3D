12f8f63482fa85ca0fba7948bdc2c3c6e123e004
harry <nguyenvantien060604@gmail.com>
2025-11-09 02:29:54 +0700

    version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: '3D Viewer Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF007BFF), // Xanh biển
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF007BFF), // Xanh biển
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: const ViewerPage(),
     );
   }
@@ -26,20 +39,133 @@ class ViewerPage extends StatefulWidget {
 }
 
 class _ViewerPageState extends State<ViewerPage> {
-  final Flutter3DController _controller = Flutter3DController();  
+  final Flutter3DController controller = Flutter3DController();
+
+  bool showInfo = true;
+
+  bool isLoading = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('3D Viewer')),
-      body: Center(
-        child: Flutter3DViewer(
-          controller: _controller,
-          // Dùng model GLB mẫu có sẵn online
-          // src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
-          src: 'assets/3d/sub.glb',
-          enableTouch: true,
+    final size = MediaQuery.of(context).size;
+    double viewerWidth = size.width * 0.92;
+    double viewerHeight = (size.width < 600) ? size.width : 500;
+
+    final viewer = ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        width: viewerWidth,
+        height: viewerHeight,
+        color: Theme.of(
+          context,
+        ).colorScheme.surfaceContainerHighest,
+        child: Stack(
+          children: [
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: Flutter3DViewer(
+                src: r'assets\3d\DamagedHelmet.glb',
+                controller: controller,
+                enableTouch: true,
+                progressBarColor: Colors.orange,
+                onLoad: (String modelAddress) =>
+                    setState(() => isLoading = false),
+              ),
+            ),
+            Positioned(
+              // file name
+              left: 12,
+              top: 12,
+              child: AnimatedOpacity(
+                opacity: showInfo ? 1 : 0,
+                duration: const Duration(milliseconds: 250),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Text(
+                    "This is the file name",
+                    style: TextStyle(color: Colors.white, fontSize: 13),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Visibility(
+                visible: isLoading,
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        width: 18,
+                        height: 18,
+                        child: CircularProgressIndicator(strokeWidth: 2),
+                      ),
+                      SizedBox(width: 12),
+                      Text(
+                        "Loading model ...",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('3D Viewer'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.people))],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: viewer,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: 'toggleInfo',
+            onPressed: () => setState(() => showInfo = !showInfo),
+            tooltip: "Toggle Info",
+            child: Icon(showInfo ? Icons.visibility : Icons.visibility_off),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: 'resetView',
+            onPressed: () {
+              controller.resetCameraOrbit();
+            },
+            tooltip: "Reset View",
+            child: Icon(Icons.center_focus_strong),
+          ),
+        ],
       ),
     );
   }
